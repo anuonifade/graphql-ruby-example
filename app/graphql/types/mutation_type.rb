d9c4b90c2dfb72619858d8bc9e9224a80e3cdb5e
andela-aonifade <anuoluwapo.onifade@andela.com>
2020-05-27 01:45:01 +0100

    adds authprovider create user to mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
+    field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
   end
 end
